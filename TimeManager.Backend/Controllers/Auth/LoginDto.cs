81d615404ef0df1506f5e6098e0369dcad3dbc58
alsongarbuja <magar33alson@gmail.com>
2026-05-29 14:27:12 -0500

    test: simple login post controller testing

---
+++ TimeManager.Backend/Controllers/Auth/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Backend.Controllers.Auth
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be more than 100 letters")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be atleast 8 characters long")]
+        public string Password { get; set; } = string.Empty;
+    }
+}

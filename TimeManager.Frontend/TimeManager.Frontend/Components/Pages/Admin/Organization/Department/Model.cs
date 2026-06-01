129f8fbd3f0a3f4855d60e727fed01d37c9836a1
alsongarbuja <magar33alson@gmail.com>
2026-06-01 14:35:32 -0500

    WIP: Basic UI for all models till now for CRUD Operation

---
+++ TimeManager.Frontend/TimeManager.Frontend/Components/Pages/Admin/Organization/Department/Model.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Frontend.Components.Pages.Admin.Organization.Department
+{
+    public class DepartmentModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string Description { get; set; } = "";
+    }
+}
---
+++ TimeManager.Frontend/TimeManager.Frontend/Components/Pages/Admin/Organization/Unit/UnitModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Frontend.Components.Pages.Admin.Organization.Unit
+{
+    public class UnitModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue, ErrorMessage="Index must be greater than 0"), Required(ErrorMessage = "Index is required")]
+        public int Index { get; set; }
+
+        [StringLength(100, ErrorMessage = "Description cannot be longer than 100 characters")]
+        public string Description { get; set; } = "";
+    }
+}

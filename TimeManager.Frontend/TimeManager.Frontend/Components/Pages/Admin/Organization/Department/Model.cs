355436f173e1fd8cc7fc8b12e3164def6e31d6cb
alsongarbuja <magar33alson@gmail.com>
2026-06-02 14:21:49 -0500

    icon: add icon and WIP update, delete action in list

--- TimeManager.Frontend/TimeManager.Frontend/Components/Pages/Admin/Organization/Department/Model.cs
+++ TimeManager.Frontend/TimeManager.Frontend/Components/Pages/Admin/Organization/Department/Model.cs
@@ -4,6 +4,8 @@ namespace TimeManager.Frontend.Components.Pages.Admin.Organization.Department
 {
     public class DepartmentModel
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; } = string.Empty;
 

bdefd55d9159a7dcbc638b8acec53fa67fdea8be
alsongarbuja <magar33alson@gmail.com>
2026-06-23 13:53:33 -0500

    utils: password toggle, small ui fixes

--- TimeManager.Backend/TagHelpers/CustomInputTagHelper.cs
+++ TimeManager.Backend/TagHelpers/CustomInputTagHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace TimeManager.Backend.TagHelpers
@@ -8,6 +9,10 @@ namespace TimeManager.Backend.TagHelpers
     {
         [HtmlAttributeName("asp-for")]
         public ModelExpression For { get; set; }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
@@ -22,9 +27,27 @@ namespace TimeManager.Backend.TagHelpers
             var propertyValue = For.Model?.ToString() ?? string.Empty;
             var type = For.Metadata.DataTypeName == "Password" ? "password" : "text";
 
+            var hasErrors = ViewContext.ModelState.TryGetValue(propertyName, out var modelStateEntry) && modelStateEntry.Errors.Any();
+
+            var errorMessage = hasErrors ? modelStateEntry.Errors.First().ErrorMessage : string.Empty;
+
             var input = isMultiLine
                 ? $"<textarea name='{propertyName}' value='{propertyValue}' class='form-input'></textarea>"
                 : $"<input name='{propertyName}' {inputIsRequired} value='{propertyValue}' class='form-input' type='{type}' />";
+
+            if (type == "password")
+            {
+                input = $"<div class='password-toggle-wrapper'>" +
+                            $"{input}" +
+                            $"<button type='button' class='password-toggle-btn' onclick='togglePasswordVisibility(this)'>" +
+                                $"<i class='ri-eye-off-line'></i>" +
+                            $"</button>" +
+                        $"</div>";
+            }
+
+            var errorSpan = hasErrors
+                   ? $"<span>{errorMessage}</span>"
+                   : string.Empty;
 
             output.TagName = null;
 
@@ -32,6 +55,7 @@ namespace TimeManager.Backend.TagHelpers
                 $"<div class='form-group'>" +
                     $"<label class='form-label'>{labelText}{required}</label>" +
                     $"<div class='form-input-wrapper'>{input}</div>" +
+                    $"{errorSpan}" +
                 $"</div>"
             );
         }

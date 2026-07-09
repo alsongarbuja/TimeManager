c39c58ef68b5ac98b3b4eaebf96614bd3c29c9d4
alsongarbuja <magar33alson@gmail.com>
2026-07-09 11:55:17 -0500

    feat: modal in admin layout

---
+++ TimeManager.Backend/ViewModels/BulkCreateModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Backend.ViewModels
+{
+    public class BulkCreateModel
+    {
+        [Required(ErrorMessage = "File is required")]
+        [Display(Name = "File")]
+        public IFormFile ExcelFile { get; set; }
+    }
+}

01f6b42b3873210744ed335f2ba7b7090b958528
alsongarbuja <magar33alson@gmail.com>
2026-07-14 15:15:54 -0500

    chore: WIP remove unwanted codes and coding convention enforcement

---
+++ TimeManager.Backend/ViewModels/PartialViews/ReportTablePartialViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace TimeManager.Backend.ViewModels.PartialViews
+{
+    public class ReportTablePartialViewModel
+    {
+        public double TotalHours { get; set; } = 0.0;
+        public ReportTableWeekRow WeekOne { get; set; }
+        public ReportTableWeekRow WeekTwo { get; set; }
+    }
+
+    public class ReportTableWeekRow
+    {
+        public string Label { get; set; } = string.Empty;
+        public Dictionary<string, DayReport> Week { get; set; }
+    }
+
+    public class ReportStatModel 
+    {
+        public double TotalHours { get; set; } = 0.0;
+        public double TotalWorkedHours { get; set; } = 0.0;
+        public double TotalHolidayHours { get; set; } = 0.0;
+    }
+}

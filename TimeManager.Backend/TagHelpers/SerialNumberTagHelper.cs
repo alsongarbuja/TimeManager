5255f2474fbd625f3a2efb66b7788d90b91474d7
alsongarbuja <magar33alson@gmail.com>
2026-07-20 09:32:55 -0500

    feat: add serial number to data table using custom tag helper partial view

---
+++ TimeManager.Backend/TagHelpers/SerialNumberTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TimeManager.Backend.TagHelpers
+{
+    [HtmlTargetElement("sn-cell")]
+    public class SerialNumberTagHelper : TagHelper
+    {
+        [HtmlAttributeName("counter-name")]
+        public string CounterName { get; set; } = "default";
+
+        [HtmlAttributeName("start")]
+        public int Start { get; set; } = 0;
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; } = default!;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var key = $"SnCounter_{CounterName}";
+            var items = ViewContext.HttpContext.Items;
+
+            int current = 0;
+            //int current = items.TryGetValue(key, out var val) ? (int)val! : 0;
+            if (!items.ContainsKey(key))
+            {
+                current = Start + 1;
+            }
+            else
+            {
+                current = (int)items[key]! + 1;
+            }
+            items[key] = current;
+
+            output.TagName = null;
+            output.Content.SetHtmlContent(
+                $"<td class='table-cell'>" +
+                current.ToString() +
+                $"</td>"
+            );
+        }
+    }
+}
